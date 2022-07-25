3d258d346be6c779847f62442701feed8217b640
zhangdongwei <zhangdongwei@bytedance.com>
2022-07-25 15:49:17 +0800

    add six

---
+++ project/six.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+/**
+* Author	: zhangdongwei@bytedance.com
+* Time		: 2022-07-25
+ * 函数 及返回值
+ *
+*/
+func main() {
+	res := plus(1, 2)
+	fmt.Println("1+2=", res)
+
+	res = plusPlus(1, 2, 3)
+	fmt.Println("1+2+3=", res)
+}
+
+func plus(a int, b int) int {
+	return a + b
+}
+
+func plusPlus(a, b, c int) int {
+	return a + b + c
+}
